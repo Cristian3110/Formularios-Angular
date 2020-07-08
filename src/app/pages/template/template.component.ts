a8383a812b7ac14167b3c535426269706b388ecf
Makoto3110 <cristiancarrillo311087@gmail.com>
2020-07-08 01:13:37 -0400

    tarea repaso de las validaciones y lista la resolución de la misma

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -9,6 +9,8 @@ import { NgForm } from '@angular/forms';
 export class TemplateComponent implements OnInit {
   usuario = {
     nombre: '',
+    apellido: '',
+    correo: '',
   };
 
   constructor() {}
